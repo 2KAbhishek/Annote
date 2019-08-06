717361635311cce31cfbcca40753b9b7df62f3e1
Abhishek Keshri <keshri.abhishek63@gmail.com>
2019-08-06 17:42:06 +0530

    Update notebook url

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NoteService {
-  notebookUrl:string = 'https://my-json-server.typicode.com/2kabhishek/Annote';
+  notebookUrl:string = 'https://my-json-server.typicode.com/2kabhishek/Annote/notes';
 
   constructor(private http:HttpClient) { }
 
