0e501ef6946e0cf5a05410d332f63ac86c60e37a
Abhishek Keshri <keshri.abhishek63@gmail.com>
2019-08-06 19:44:31 +0530

    Add header component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotebookComponent } from './components/notebook/notebook.component';
 import { NoteComponent } from './components/note/note.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotebookComponent,
-    NoteComponent
+    NoteComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
