c6b6fd9f8443b24bfb83d8af2ce02f999f80664c
Abhishek Keshri <keshri.abhishek63@gmail.com>
2019-08-07 06:06:08 +0530

    Routing added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { NotebookComponent } from 'src/app/components/notebook/notebook.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NotebookComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
